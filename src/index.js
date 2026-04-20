f28870554dd3b71add29edae02ac6f365e226a09
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-20 15:40:39 +0500

    🚀 Add main application code

---
+++ src/index.js
@@ -0,0 +1,347 @@
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
+```
+
+```javascript
+function topKFrequent(nums, k) {
+    let count = {};
+    for (let num of nums) {
+        if (count[num]) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return Object.keys(count).sort((a, b) => count[b] - count[a]).slice(0, k);
+}
